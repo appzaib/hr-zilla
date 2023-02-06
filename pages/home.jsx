f8e145bb390b1e7a9f646c00fbe43ff2f8569173
Nadiakhanv2 <nadiaiqbal2600@gmail.com>
2023-02-05 22:42:47 -0800

    home

---
+++ pages/home.jsx
@@ -0,0 +1,118 @@
+import {GiExpense,GiControlTower,GiLightBulb,GiTeamIdea } from 'react-icons/gi';
+import {FaQuoteRight } from 'react-icons/fa';
+
+
+const Feature = () => {
+  return (
+    <div className='container text-gray-600'>
+
+    <div className=" px-5 py-5 mx-auto">
+      <div className="flex flex-col text-center w-full mb-4">
+        <h1 className="sm:text-3xl text-2xl font-medium  text-red-900">You Will Recieve</h1>
+        </div>
+      <div className="flex flex-wrap">
+        <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-red-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-purple-900 font-medium  mb-2">Key account manager</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          </div>
+          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-red-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-purple-900 font-medium  mb-2">Weekly Reports</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          </div>
+          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-red-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-purple-900 font-medium  mb-2">Advice and Support</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          </div>
+          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-red-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-purple-900 font-medium  mb-2">Candidates with proven track record</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          </div>
+      </div>
+      </div>
+  
+      <div className=" bg-gradient-to-r from-red-400 to-purple-100">
+          <div className="text-center w-full">
+          <h1 className="title-font sm:text-4xl text-3xl pt-4 font-medium text-white">CUSTOMER BENEFITS:</h1>
+        </div>
+      
+    <div className=" px-5 py-4 mx-auto flex flex-wrap">
+      
+      <div className="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
+        <div className="h-full w-6 absolute  flex items-center justify-center">
+          <div className="h-full w-1 bg-white pointer-events-none"></div>
+        </div>
+        <div className="flex-shrink-0 w-6 h-6 rounded-full mt-5 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+        <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+          <div className="flex-shrink-0 w-16 h-16 bg-white text-purple-500 rounded-full inline-flex items-center justify-center">
+            <GiExpense className=' w-10 h-10'/>
+          </div>
+          <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+            <h2 className="font-medium title-font text-yellow-800 mb-1 text-xl">No additional expenses</h2>
+            <p className="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
+        <div className="h-full w-6 absolute  flex items-center justify-center">
+          <div className="h-full w-1 bg-white pointer-events-none"></div>
+        </div>
+        <div className="flex-shrink-0 w-6 h-6 rounded-full mt-5 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">2</div>
+        <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+          <div className="flex-shrink-0 w-16 h-16 bg-white text-purple-500 rounded-full inline-flex items-center justify-center">
+            <GiControlTower className=' w-10 h-10'/>
+          </div>
+          <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+            <h2 className="font-medium title-font text-yellow-800 mb-1 text-xl">Full control</h2>
+            <p className="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
+        <div className="h-full w-6 absolute  flex items-center justify-center">
+          <div className="h-full w-1 bg-white pointer-events-none"></div>
+        </div>
+        <div className="flex-shrink-0 w-6 h-6 rounded-full mt-5 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">3</div>
+        <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+          <div className="flex-shrink-0 w-16 h-16 bg-white text-purple-500 rounded-full inline-flex items-center justify-center">
+            <GiLightBulb className=' w-10 h-10'/>
+          </div>
+          <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+            <h2 className="font-medium title-font text-yellow-800 mb-1 text-xl">Better solutions</h2>
+            <p className="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
+        <div className="h-full w-6 absolute  flex items-center justify-center">
+          <div className="h-full w-1 bg-white pointer-events-none"></div>
+        </div>
+        <div className="flex-shrink-0 w-6 h-6 rounded-full mt-5 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">4</div>
+        <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+          <div className="flex-shrink-0 w-16 h-16 bg-white text-purple-500 rounded-full inline-flex items-center justify-center">
+            <GiTeamIdea className=' w-10 h-10'/>
+          </div>
+          <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+            <h2 className="font-medium title-font text-yellow-800 mb-1 text-xl">Complete team of developers</h2>
+            <p className="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+  
+  </div>
+  <div className="px-5 py-5 mx-auto ">
+      <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+        <FaQuoteRight className="inline-block w-8 h-8 text-purple-400 mb-8"/>
+        <p className="leading-relaxed text-lg">Hiring Hrzilaa is one of the best decisions.We've made in the history of our company recommending them is 1000% a no-brainer.Would'nt hire anyone else</p>
+        <span className="inline-block h-1 w-10 rounded bg-purple-500 mt-8 mb-6"></span>
+        <h2 className="text-gray-900 font-medium  tracking-wider text-sm">Nadya Khan</h2>
+        <p className="text-gray-500"> Product Designer</p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Feature
